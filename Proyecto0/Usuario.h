f987c5e013d16687f89242fb6ccf7f0af358d31d
Isaacgithub123 <isaacazofeifab@gmail.com>
2026-05-07 19:24:50 -0600

    Usuario completado(1)

--- Proyecto0/Usuario.h
+++ Proyecto0/Usuario.h
@@ -13,6 +13,13 @@ public:
     Usuario() : descripcion(""), prioridad(999) {} // Constructor default
     Usuario(const string& desc, int prior) : descripcion(desc), prioridad(prior) {}  // Constructor con parámetros 
    
-    // Destructor
-    ~Usuario() {}
+    ~Usuario() {}// Destructor
+    // Getters
+    string getDescripcion() const { return descripcion; }
+    int getPrioridad() const { return prioridad; 
+    // Setters
+    void setDescripcion(const string& desc) { descripcion = desc; }
+    void setPrioridad(int prior) { prioridad = prior; }
+    }
+};
 
